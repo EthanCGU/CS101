ca4b132cbdecba21b50dcfc3bb0707a858bd034c
EthanCGU <B1329038@cgu.edu.tw>
2024-12-05 15:31:49 +0800

    Update main4.c

--- ex3/main4.c
+++ ex3/main4.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
 
+int main() {
+    int i = 2100; // 可以改為其他年份
+    if (i % 400 == 0 || (i % 4 == 0 && i % 100 != 0)) {
+        printf("閏年\n");
+    } else {
+        printf("不是閏年\n");
+    }
+    return 0;
+}
