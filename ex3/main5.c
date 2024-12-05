5886d057f4bd8765fd2e3648a1f5957569487dac
EthanCGU <B1329038@cgu.edu.tw>
2024-12-05 15:32:35 +0800

    Update main5.c

--- ex3/main5.c
+++ ex3/main5.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i = 119; // 可更改為任意值
+    if (i <= 30) {
+        printf("免費\n");
+    } else if (i >= 240) {
+        printf("240元\n");
+    } else {
+        if (i % 30) {
+            int h = ((i / 30) + 1) * 30;
+            printf("%d 元\n", h);
+        } else {
+            printf("%d 元\n", i);
+        }
+    }
+    return 0;
+}
 
