530d937ffdac7243af6a81c60c584287eb374d83
EthanCGU <B1329038@cgu.edu.tw>
2024-12-11 21:20:03 +0800

    Update main3.c

--- hw5/main3.c
+++ hw5/main3.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int x = 1, y = 1;
+    for(int i = 1; i <= 81; i++){
+        printf("%d*%d=%d\t", y, x, x*y);
+        if(i%9 == 0){
+            printf("\n");
+            x = 1;
+            y++;
+        }
+        else{
+            x++;
+        }
+    }
+    return 0;
+}
 
