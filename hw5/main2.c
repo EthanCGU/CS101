02d6226c74cb04c7a3dde31b90730e8f355297bf
EthanCGU <B1329038@cgu.edu.tw>
2024-12-11 21:03:53 +0800

    Update main2.c

--- hw5/main2.c
+++ hw5/main2.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+
+int main(){
+    double PI = 4.0, t;
+    int i;
+    for(i = 1; ; i++){
+        t = 4.0 / (2 * i + 1);
+        if(i%2 == 1){
+            PI = PI - t;
+        }
+        else{
+            PI = PI + t;
+        }
+
+        if(PI >= 3.141585 && PI < 3.141595){
+            break;
+        }
+    }
+    int x = i + 1;
+
+    printf("%d", x);
+}
 
