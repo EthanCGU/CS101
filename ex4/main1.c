57dee4733f6ecaa3e571b9f5fadd02b1103381e8
EthanCGU <B1329038@cgu.edu.tw>
2024-12-12 15:21:44 +0800

    Update main1.c

--- ex4/main1.c
+++ ex4/main1.c
@@ -1,1 +1,17 @@
+#include<stdio.h>
 
+int main(){
+    int n = 7; 
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j<n-i+1){
+                printf(" ");
+            }
+            else{
+                printf("%d ", i);
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
