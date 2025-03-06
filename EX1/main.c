a9fc056b4e4f869fe17c2a3ef7af7b8e306d6eb9
EthanCGU <B1329038@cgu.edu.tw>
2025-03-06 16:00:32 +0800

    Update main.c

--- EX1/main.c
+++ EX1/main.c
@@ -1,38 +1,36 @@
 #include <stdio.h>
 
 int main()
-{
-    FILE* fp;
-    int a[] = {0, 1, 2};
+{   
+    FILE * fp;
+    int a[] = {0, 1 , 2};
     char b[] = "ABC";
-    float c[] = {1.1, 1.2, 1.3};
-    int ra[4];
-    char rb[4];
-    float rc[4];
+    float c[] = {1.1,1.2,1.3};
+    int ra[3];
+    char rb[3];
+    float rc[3];
+    fp = fopen ("a.bin", "wb+");
+    fwrite(a,sizeof (int),3,fp);
+    fwrite(b,sizeof (char),3,fp);
+    fwrite(c,sizeof (float),3,fp);
     
-    fp = fopen("a.bin", "wb+");
-    fwrite(a, sizeof(a), 1, fp);
-    fseek(fp, 0, SEEK_SET);
-    fread(ra, sizeof(ra), 1, fp);
-    fwrite(b, sizeof(b), 1, fp);
-    fseek(fp, 0, SEEK_SET);
-    fread(rb, sizeof(rb), 1, fp);
-    fwrite(c, sizeof(c), 1, fp);
-    fseek(fp, 0, SEEK_SET);
-    fread(rc, sizeof(rc), 1, fp);
+    rewind(fp);
     
-    for(int i=0; i<3; i++){
-        printf("%d", ra[i]);
+    fread(ra,sizeof (int),3,fp);
+    fread(rb,sizeof (char),3,fp);
+    fread(rc,sizeof (float),3,fp);
+    for(int i=0;i<3;i++){
+        printf("%d\t",ra[i]);
     }
     printf("\n");
-    for(int i=0; i<3; i++){
-        printf("%c", rb[i]);
+    for(int i=0;i<3;i++){
+        printf("%c\t",rb[i]);
     }
     printf("\n");
-    for(int i=0; i<3; i++){
-        printf("%f", rc[i]);
+    for(int i=0;i<3;i++){
+        printf("%.1f\t",rc[i]);
     }
     printf("\n");
-
+    fclose(fp);
     return 0;
 }
